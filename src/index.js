d3cd5008d29405d074d893789006610647607f75
Rodion <82043199+RodionKipitok@users.noreply.github.com>
2024-02-14 13:33:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from "components/App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import "./index.css";
-import { StrictMode } from "react";
+
 
 // После создания строго необходимо связать его с компонентами React, чтобы они могли получать доступ к стору и его методам. Для этого в библиотеке React Redux есть компонент  Provider , ожидающий одноименный пропс  store. Для того чтобы любой компонент в приложении мог использовать стр., вращаем  Provider все дерево компонентов.
 
